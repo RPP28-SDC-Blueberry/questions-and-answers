e222ff5b6da616812ac09146fffd5e98467ac6cc
Kingsley Allen <kingsley.allen@gmail.com>
2021-08-06 21:50:34 -0500

    tests

---
+++ db/testHelper.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const Question = require('./models.js')
+const db = require('./connection.js')
+
+// TESTING HELPERS
+
+async function deleteAllQuestionsForProductId(product_id) {
+  try {
+    let result = await Question.deleteMany({product_id: product_id})
+    return result;
+  } catch (error) {
+    return error;
+  }
+}
+
+module.exports = {
+  deleteAllQuestionsForProductId,
+};
--- tests/api.test.js
+++ tests/api.test.js
@@ -3,18 +3,20 @@ const app = require('../server/api.js')
 const supertest = require('supertest')
 const request = supertest(app);
 const mongoose = require('mongoose')
+const helper = require('../db/testHelper.js')
 
-after(function() {
+after(async function() {
+  const response = await helper.deleteAllQuestionsForProductId('500100');
   mongoose.connection.close();
 });
 
 // For unit/integration tests, we'll operate in the 500000 set of product_ids
-// Range for reads: 500000 - 500100; no updates to be made here to ensure stable results
+// Reads:   500000 - 500099 -- no updates to be made here to ensure stable results
+// Creates: 500100 - 500199 -- creates and updates here
 
+describe('List questions', function() {
 
-describe('Reads', function() {
-
-  // For reads, we'll leave the 500000 - 500100 range alone for stable counts
+  // Read range: 500000 - 500099
   // Eventually, we'll build a test db that's seeded every run
 
   it('Should be 3 questions returned for product_id 500000', async function () {
@@ -55,10 +57,9 @@ describe('Reads', function() {
   })
 })
 
-describe('Answer reads', function() {
+describe('List answers', function() {
 
   it('Should be 2 answer returned for a specific question', async function () {
-
     const questionId = '610655218d6f85d5b6a1f1fe';
     const response = await request
       .get(`/qa/questions/${questionId}/answers`)
@@ -75,9 +76,74 @@ describe('Answer reads', function() {
   })
 });
 
-// describe('Create ', function() {
-//   // app.post('/qa/questions', async (req, res, next) => {
-// });
+
+describe('Adds and updates', function() {
+
+  // Write range: 500100 - 500199
+  // Eventually, we'll build a test db that's seeded every run
+
+  it('Add a question', async function () {
+
+    const newQuestion = {
+      "product_id": '500100',
+      "body": "Is it from France?",
+      "name": "lincoln",
+      "email": "iamlincoln@mailinator.com"
+    };
+
+    const postedQuestion = await request
+      .post('/qa/questions')
+      .send(newQuestion)
+    expect(postedQuestion.status).to.equal(201);
+    expect(postedQuestion.body).to.be.empty;
+
+    const findQuestion = await request
+      .get('/qa/questions')
+      .query({ product_id: '500100'})
+    expect(findQuestion.status).to.equal(200);
+    expect(findQuestion.body.product_id).to.equal('500100');
+    expect(findQuestion.body.results).to.have.lengthOf(1);
+  })
+
+  it('Add an answer', async function () {
+
+    // find the question to add an answer to
+    const foundQuestion = await request
+      .get('/qa/questions')
+      .query({ product_id: '500100'})
+    expect(foundQuestion.status).to.equal(200);
+    expect(foundQuestion.body.product_id).to.equal('500100');
+    expect(foundQuestion.body.results).to.have.lengthOf(1);
+
+    // post the answer
+    const questionId = foundQuestion.body.results[0].question_id;
+    const newAnswer = {
+      "body": "No it's from space.",
+      "name": "douglas",
+      "email": "doug.e.fresh@mailinator.com",
+      "photos": [
+        "https://dummyimage.com/200x154.png/111/aa",
+        "https://dummyimage.com/259x238.png/222/bb",
+        "https://dummyimage.com/783x522.png/333/cc"
+      ]
+    };
+    const postedAnswer = await request
+      .post(`/qa/questions/${questionId}/answers`)
+      .send(newAnswer)
+    expect(postedAnswer.status).to.equal(201);
+    expect(postedAnswer.body).to.be.empty;
+
+    // get the answer that was posted
+    const foundAnswer = await request
+      .get(`/qa/questions/${questionId}/answers`)
+    expect(foundAnswer.status).to.equal(200);
+    expect(foundAnswer.body.question).to.equal(questionId);
+    expect(foundAnswer.body.results).to.have.lengthOf(1);
+    expect(foundAnswer.body.results[0].photos).to.have.lengthOf(3);
+    expect(foundAnswer.body.results[0].answerer_name).to.equal('douglas');
+  })
+
+});
 
 
 
