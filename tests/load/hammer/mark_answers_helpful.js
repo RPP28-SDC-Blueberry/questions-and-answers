6d2f36bb5011d7f27c3c57cf2422911b33e63e54
Kingsley Allen <kingsley.allen@gmail.com>
2021-08-10 16:40:50 -0500

    running k6 tests, added paging, added index to answer subdoc array (seems it should be automatically created based on the mongoose docs, but it was not)

--- tests/load/hammer/mark_answers_helpful.js
+++ tests/load/hammer/mark_answers_helpful.js
@@ -6,11 +6,11 @@ export let options = {
   scenarios: {
     constant_request_rate: {
       executor: 'constant-arrival-rate',
-      rate: 1,
+      rate: 10000,
       timeUnit: '1s', // 1000 iterations per second, i.e. 1000 RPS
       duration: '30s',
-      preAllocatedVUs: 500, // how large the initial pool of VUs would be
-      maxVUs: 1500, // if the preAllocatedVUs are not enough, we can initialize more
+      preAllocatedVUs: 2500, // how large the initial pool of VUs would be
+      maxVUs: 2500, // if the preAllocatedVUs are not enough, we can initialize more
     },
   },
 };
