4cba41584568d79882e9a10d5337aeb383b1f87c
Kingsley Allen <kingsley.allen@gmail.com>
2021-08-28 00:48:14 -0500

    adding redis

--- server/api.js
+++ server/api.js
@@ -3,13 +3,10 @@ const app = express()
 
 const bodyParser = require('body-parser')
 const db = require('../db/queries.js')
+const redis = require('./redis.js')
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
-
-app.get('/loaderio-7c9e36043b78a137b019adef8c409c2e', (req, res, next) => {
-  res.send('loaderio-7c9e36043b78a137b019adef8c409c2e');
-})
 
 // list questions
 app.get('/qa/questions', async (req, res, next) => {
@@ -18,18 +15,44 @@ app.get('/qa/questions', async (req, res, next) => {
     if (!productId) {
       res.status(404).send();
     } else {
-      let pageNumber = !req.query.page ? 1 : Math.max(Number(req.query.page), 1);
-      let itemsPerPage = !req.query.count ? 5 : Math.max(Number(req.query.count), 0);
-      const productQuestions = await db.listQuestions(productId, pageNumber, itemsPerPage);
-      let responseBody = {};
-      responseBody.product_id = productId;
-      responseBody.results = productQuestions;
-      res.send(responseBody);
+      const redisRecord = await redis.get(req.url);
+      if (redisRecord) {
+        res.send(JSON.parse(redisRecord));
+      } else {
+        let pageNumber = !req.query.page ? 1 : Math.max(Number(req.query.page), 1);
+        let itemsPerPage = !req.query.count ? 5 : Math.max(Number(req.query.count), 0);
+        const productQuestions = await db.listQuestions(productId, pageNumber, itemsPerPage);
+        let responseBody = {};
+        responseBody.product_id = productId;
+        responseBody.results = productQuestions;
+        const setRedis = await redis.set(req.url, JSON.stringify(responseBody));
+        res.send(responseBody);
+      }
     }
-  } catch (error) {
+   } catch (error) {
     next(error)
   }
 });
+
+// // list questions
+// app.get('/qa/questions', async (req, res, next) => {
+//   try {
+//     let productId = req.query.product_id;
+//     if (!productId) {
+//       res.status(404).send();
+//     } else {
+//       let pageNumber = !req.query.page ? 1 : Math.max(Number(req.query.page), 1);
+//       let itemsPerPage = !req.query.count ? 5 : Math.max(Number(req.query.count), 0);
+//       const productQuestions = await db.listQuestions(productId, pageNumber, itemsPerPage);
+//       let responseBody = {};
+//       responseBody.product_id = productId;
+//       responseBody.results = productQuestions;
+//       res.send(responseBody);
+//     }
+//   } catch (error) {
+//     next(error)
+//   }
+// });
 
 // list answers
 app.get('/qa/questions/:question_id/answers', async (req, res, next) => {
@@ -121,6 +144,24 @@ app.put('/qa/answers/:answer_id/report', async (req, res, next) => {
     next(error)
   }
 });
+
+// loaderio endpoint
+app.get('/loaderio*', (req, res, next) => {
+  res.send(req.url.substring(1, req.url.length - 1));
+})
+
+// test redis set
+app.get('/redistest/:key/:value', async (req, res, next) => {
+  try {
+    let key = req.params.key;
+    let value = req.params.value;
+    const setResponse = await redis.set(key, value);
+    const getResponse = await redis.get(key);
+    res.send(getResponse);
+  } catch (error) {
+    next(error)
+  }
+})
 
 // Invalid route yields a 404
 app.use(function (req, res, next) {
